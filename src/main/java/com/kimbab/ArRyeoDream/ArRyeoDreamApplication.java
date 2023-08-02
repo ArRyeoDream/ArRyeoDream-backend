a2bbf05a72aebf9514388bbbf00d80104e44904e
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-08-02 20:34:51 +0900

    Init project

---
+++ src/main/java/com/kimbab/ArRyeoDream/ArRyeoDreamApplication.java
@@ -0,0 +1,13 @@
+package com.kimbab.ArRyeoDream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArRyeoDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArRyeoDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimbab/ArRyeoDream/ArRyeoDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimbab.ArRyeoDream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArRyeoDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
